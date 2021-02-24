8b16395536ba98ffe4a95dc1cf9060a3dd8f8636
Santiago Echeverri <santiago.echeverri@pragma.com.co>
2021-02-24 16:22:29 -0500

    cambiuos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import React from 'react';
 import Marcas from './services/prueba';
-import Login from './components/pages/login'
+import Login from './components/pages/login';
+import Amplify, { Auth } from 'aws-amplify';
 
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+
+Amplify.configure({
+  region: 'xx-xxxxx-x',
+})
 
 function App() {
   return (
